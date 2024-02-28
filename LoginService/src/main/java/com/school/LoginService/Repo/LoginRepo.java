19d57f90e58cf3d462c23dcd948236505035e252
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2024-02-28 18:11:23 +0530

    Delete LoginService/src/main/java/com/school/LoginService/Repo/LoginRepo.java

--- LoginService/src/main/java/com/school/LoginService/Repo/LoginRepo.java
+++
@@ -1,9 +0,0 @@
-package com.school.LoginService.Repo;
-
-import com.school.LoginService.Model.LoginModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LoginRepo extends JpaRepository<LoginModel, Integer> {
-
-    LoginModel findByEmail(String email);
-}
