840aaead98ad5a19ee63f2efa309c7df1b53f0d5
Girjesh <107177466+GirjeshBaghel@users.noreply.github.com>
2024-02-29 15:03:26 +0530

    Update RouteValidator.java

--- ApiGateway/src/main/java/com/school/ApiGateway/Filter/RouteValidator.java
+++ ApiGateway/src/main/java/com/school/ApiGateway/Filter/RouteValidator.java
@@ -9,13 +9,44 @@ import java.util.function.Predicate;
 @Component
 public class RouteValidator {
 
-    public static final List<String> openApiEndpoints = List.of(
+   public static final List<String> openApiEndpoints = List.of(
             "/login/getotp",
             "/login/all",
             "admin/superadmin/register",
             "/eureka/**",
-            "/request/my-request",
-            "/sch/addSchool"
+            "/admin/**",
+            "/fee/**",
+            "/events/**",
+            "/sch/addSchool",
+            "/sch/getAllSchool",
+            "/sch/getById",
+            "/sch/editById",
+            "/sch/deleteById",
+            "/sch/addPlanInSchool",
+            "/sch/addSpSerBySchId",
+            "/sch/getSpFeaBySchId",
+            "/plan/addPlans",
+            "/plan/getAllPlan",
+            "/plan/getById",
+            "/plan/editById",
+            "/plan/deleteById",
+            "/fea/addFeaByPlanId",
+            "/fea/getAllFeatures",
+            "/fea/getAllFeaByPlanId",
+            "/fea/getByFeaId",
+            "/fea/editByfeaId",
+            "/fea/deleteById",
+            "per/addPermission",
+            "per/getAllPermission",
+            "per/getAllPerByFeaId",
+            "per/getById",
+            "per/editById",
+            "per/deleteById",
+            "/subs/getAllSubs",
+            "/subs/editBySubsId",
+           "/swagger-ui/index.html"
+
+
     );
 
     public Predicate<ServerHttpRequest> isSecured =
